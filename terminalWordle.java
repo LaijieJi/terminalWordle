7acd14fb277e5c0b6932501518e663c80e79ef1e
Laijie <ji.laijie@gmail.com>
2024-01-28 18:06:29 +0100

    ENH: Loop added

--- terminalWordle.java
+++ terminalWordle.java
@@ -10,6 +10,8 @@ class TerminalWordle{
 
     public static final String RESET = "\033[0m";
     public static final String RED_BACKGROUND = "\033[41m";
+    public static final String RED = "\033[0;31m";
+    public static final String GREEN = "\033[0;32m";
     public static final String GREEN_BACKGROUND = "\033[42m";
     public static final String BLUE_BACKGROUND = "\033[44m";
     public static final String YELLOW_BACKGROUND = "\033[43m";
@@ -24,38 +26,56 @@ class TerminalWordle{
             System.out.println(BOLD + "<--Welcome to the terminal Wordle!-->" + RESET);
             System.out.println(BOLD + "<--Let's start!-->" + RESET);
 
-            Thread.sleep(4000);
+            Thread.sleep(3000);
 
             clearConsole();
 
-            tw.startGame();
+            while(tw.startGame()) {}
+
+            System.out.println(BOLD + "Goodbye!" + RESET);
         } catch (InterruptedException ie) {
             ie.printStackTrace();
         }
     }
 
-    public void startGame(){
+    public boolean startGame(){
         this.setWordle();
         Scanner sc = new Scanner(System.in);
-        boolean guessed = false;
 
-        String ans;
+        try {
 
-        System.out.println(BOLD + "Make your guess" + RESET);
-        for(int i = 0; i < 5 && !guessed; i++) {
-            ans = sc.nextLine().toLowerCase();
-            if(ans.length() != 5){
-                System.out.println("Wrong format, remember that your guess consists of 5 characters");
-            } else {
-                guessed = this.guess(ans);
+            boolean guessed = false;
+
+            String ans;
+
+            System.out.println(BOLD + "Make your guess" + RESET);
+            for (int i = 0; i < 5 && !guessed; i++) {
+                ans = sc.nextLine().toLowerCase();
+                if (ans.length() != 5) {
+                    System.out.println(RED + "Wrong format, remember that your guess consists of 5 characters" + RESET);
+                    i--;
+                } else {
+                    guessed = this.guess(ans);
+                }
             }
+
+            if (guessed) {
+                System.out.println(BOLD + GREEN + "Congratulations, you've nailed it!!" + RESET);
+            } else {
+                System.out.println(BOLD + RED + "Ohh, I'm sorry, you didn't guess it, the wordle was: " + getWordle() + RESET);
+            }
+
+            Thread.sleep(2000);
+
+            clearConsole();
+
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
         }
 
-        if(guessed) {
-            System.out.println(BOLD + GREEN_BACKGROUND + "Congratulations, you've nailed it!!" + RESET);
-        } else {
-            System.out.println(BOLD + BLUE_BACKGROUND + "Ohh, I'm sorry, you didn't guess it, the wordle was: " + getWordle() + RESET);
-        }
+        System.out.println(BOLD + "Do you want to try again with another wordle? (y/n)");
+
+        return sc.nextLine().equalsIgnoreCase("y");
     }
 
     public boolean guess(String ans) {
@@ -97,9 +117,7 @@ class TerminalWordle{
         System.out.flush();
     }
 
-    public TerminalWordle(){
-
-    }
+    public TerminalWordle(){ }
 
     public String getWordle() {
         return this.wordle;
