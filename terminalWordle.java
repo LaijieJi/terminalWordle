4492d4875a31ce5d00a62c6db2e383b2a1aaf5bd
Laijie <108839952+LaijieJi@users.noreply.github.com>
2024-01-30 08:56:50 +0100

    ENH: Visual Improvementes

--- terminalWordle.java
+++ terminalWordle.java
@@ -15,6 +15,8 @@ class TerminalWordle{
     public static final String GREEN_BACKGROUND = "\033[42m";
     public static final String BLUE_BACKGROUND = "\033[44m";
     public static final String YELLOW_BACKGROUND = "\033[43m";
+    public static final String YELLOW = "\u001b[33m";
+    public static final String WHITE = "\u001b[37m";
     public static final String BOLD = "\033[1m";
     public static final String FLUSH_CONSOLE = "\033[H\033[2J";
 
@@ -25,10 +27,9 @@ class TerminalWordle{
         try {
             System.out.println(BOLD + "<--Welcome to the terminal Wordle!-->" + RESET);
             System.out.println(BOLD + "<--Let's start!-->" + RESET);
-
-            Thread.sleep(3000);
-
-            clearConsole();
+            
+            tw.fetchWordle();            
+            Thread.sleep(2000);            
 
             while(tw.startGame()) {}
 
@@ -39,9 +40,8 @@ class TerminalWordle{
     }
 
     public boolean startGame(){
-        this.setWordle();
         Scanner sc = new Scanner(System.in);
-
+        clearConsole();
         try {
 
             boolean guessed = false;
@@ -50,7 +50,7 @@ class TerminalWordle{
 
             System.out.println(BOLD + "Make your guess" + RESET);
             for (int i = 0; i < 5 && !guessed; i++) {
-                ans = sc.nextLine().toLowerCase();
+                ans = sc.nextLine().toUpperCase();
                 if (ans.length() != 5) {
                     System.out.println(RED + "Wrong format, remember that your guess consists of 5 characters" + RESET);
                     i--;
@@ -65,6 +65,7 @@ class TerminalWordle{
                 System.out.println(BOLD + RED + "Ohh, I'm sorry, you didn't guess it, the wordle was: " + getWordle() + RESET);
             }
 
+            this.fetchWordle();
             Thread.sleep(2000);
 
             clearConsole();
@@ -73,7 +74,7 @@ class TerminalWordle{
             ie.printStackTrace();
         }
 
-        System.out.println(BOLD + "Do you want to try again with another wordle? (y/n)");
+        System.out.println(BOLD + "Do you want to try again with another wordle? (y/n)" + RESET);
 
         return sc.nextLine().equalsIgnoreCase("y");
     }
@@ -81,30 +82,33 @@ class TerminalWordle{
     public boolean guess(String ans) {
         char aux;
         int guessedChars = 0;
+        
+        System.out.print("\033[1A" + "\033[k" +  "|");
         for(int i = 0; i < 5; i++){
             aux = ans.charAt(i);
             if (aux == this.getWordle().charAt(i)) {
-                System.out.print(GREEN_BACKGROUND + aux + RESET);
+                System.out.print(GREEN_BACKGROUND + WHITE + BOLD + aux + RESET + "|");
                 guessedChars++;
             } else if (this.getWordle().contains("" + aux)){
-                System.out.print(YELLOW_BACKGROUND + aux + RESET);
+                System.out.print(YELLOW + BOLD + aux + RESET + "|");
             } else {
-                System.out.print(aux);
+                System.out.print(aux + "|");
             }
         }
+
         System.out.println();
 
         return (guessedChars == 5);
     }
 
-    public void setWordle() {
+    public void fetchWordle() {
         try {
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create("https://random-word-api.herokuapp.com/word?length=5"))
                     .method("GET", HttpRequest.BodyPublishers.noBody())
                     .build();
             HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-            this.wordle = response.body().toString().substring(2, 7);
+            this.wordle = response.body().toString().substring(2, 7).toUpperCase();
         } catch (IOException ioe) {
             ioe.printStackTrace();
         } catch (InterruptedException ie) {
@@ -123,5 +127,8 @@ class TerminalWordle{
         return this.wordle;
     }
 
-}
+    public void setWordle(String wordle) {
+        this.wordle = wordle;
+    }
 
+}
